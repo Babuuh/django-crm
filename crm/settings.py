f892ada1403392fd96b9bc91482b666d2ea61e37
Babuuh <46190291+Babuuh@users.noreply.github.com>
2021-04-05 19:52:51 +0300

    allowed host

--- crm/settings.py
+++ crm/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '-r7kzryf9v&tu-b)e#+p6uu4qj^u=k@#vu--_n$@=l#*qvwa&h'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['customer-management1.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
